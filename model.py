ce99b9a377fb2763ced93b48c9036f42ceb6b7ab
Jaka Vrhovnik <jaka.vrhovnik2@gmail.com>
2022-05-26 22:06:07 +0200

    Popravil ustvarjanje kart

--- model.py
+++ model.py
@@ -30,8 +30,13 @@ class Uporabnik:
 class Karta:
     povezave: list
 
-    
-
+    def zarotiraj(self, st_rotacij=1):
+        nove_povezave = self.povezave[:]
+        for _ in range(st_rotacij):
+            nove_povezave = [(num + 2) % 8 for num in nove_povezave]
+            nove_povezave = nove_povezave[-2:] + nove_povezave[:-2]
+        self.povezave = nove_povezave
+        return self
 
 
 @dataclass
@@ -46,16 +51,66 @@ class Igralec:
 class Igra:
     id_igre: int
     igralci: List[Igralec]
-    kupcek: List[Karta]
+    kupcek: List[Karta] = None
     tabela: Dict[tuple, Karta] = None
 
 
     def __post_init__(self):
         if self.tabela is None:
             self.tabela = {(x, y): None for x in range(VELIKOST_TABELE[0]) for y in range(VELIKOST_TABELE[1])}
+        if self.kupcek is None:
+            self.kupcek = []
+
+
+    @staticmethod
+    def razdeli_na_pare(seznam=[0, 1, 2, 3, 4, 5, 6, 7]):
+        assert len(seznam) % 2 == 0 # more biti sodo mnogo, sicer se ne da
+        if len(seznam) == 2:
+            yield [seznam]
+        else:
+            for i in range(1, len(seznam)):
+                nov_seznam = seznam[:]
+                nov_seznam.pop(i)
+                for pari in Igra.razdeli_na_pare(nov_seznam[1:]):
+                    novi_pari = [[seznam[0], seznam[i]]]
+                    novi_pari.extend(pari)
+                    yield novi_pari
+
+    @staticmethod
+    def pari_v_povezave(seznam_parov):
+        povezave = [None for _ in range(2 * len(seznam_parov))]
+        for par in seznam_parov:
+            povezave[par[0]] = par[1]
+            povezave[par[1]] = par[0]
+        return povezave
+
+    # @staticmethod
+    # def zarotiraj(povezave, k=1):
+    #     nove_povezave = povezave[:]
+    #     for _ in range(k):
+    #         nove_povezave = [(num + 2) % 8 for num in nove_povezave]
+    #         nove_povezave = nove_povezave[-2:] + nove_povezave[:-2]
+    #     return nove_povezave
+
+    @staticmethod
+    def vse_karte():
+        stare = []
+        for seznam_parov in Igra.razdeli_na_pare():
+            vrni = True
+            povezave = Igra.pari_v_povezave(seznam_parov)
+            karta = Karta(povezave)
+            for _ in range(4):
+                if karta in stare:
+                    vrni = False
+                karta.zarotiraj()
+            if vrni:
+                stare.append(povezave)
+                yield Karta(povezave)
+
 
     def ustvari_nov_kupcek(self):
-        pass
+        self.kupcek = [karta.zarotiraj(st_rotacij=random.randint(0,3)) for karta in Igra.vse_karte()]
+        random.shuffle(self.kupcek)
 
 
 @dataclass
@@ -64,59 +119,11 @@ class Tsuro:
     uporabniki: List[Uporabnik]
 
 
-def razdeli_na_pare(seznam=[0, 1, 2, 3, 4, 5, 6, 7]):
-    assert len(seznam) % 2 == 0 # more biti sodo mnogo, sicer se ne da
-    if len(seznam) == 2:
-        yield [seznam]
-    else:
-        for i in range(1, len(seznam)):
-            nov_seznam = seznam[:]
-            nov_seznam.pop(i)
-            for pari in razdeli_na_pare(nov_seznam[1:]):
-                novi_pari = [[seznam[0], seznam[i]]]
-                novi_pari.extend(pari)
-                yield novi_pari
-
-
-def pari_v_povezave(seznam_parov):
-    povezave = [None for _ in range(2 * len(seznam_parov))]
-    for par in seznam_parov:
-        povezave[par[0]] = par[1]
-        povezave[par[1]] = par[0]
-    return povezave
-
-
-def zarotiraj(povezave, k=1):
-    nove_povezave = povezave[:]
-    for _ in range(k):
-        nove_povezave = [(num + 2) % 8 for num in nove_povezave]
-        nove_povezave = nove_povezave[-2:] + nove_povezave[:-2]
-    return nove_povezave
-
-
-def vse_karte():
-    stare = []
-    for seznam_parov in razdeli_na_pare():
-        vrni = True
-        povezave = pari_v_povezave(seznam_parov)
-        for rotacija in range(1, 4):
-            if zarotiraj(povezave, rotacija) in stare:
-                vrni = False
-        if vrni:
-            stare.append(povezave)
-            yield povezave
-
 
 # Testni podatki:
 
-deck = [zarotiraj(karta, random.randint(0,3)) for karta in vse_karte()]
-random.shuffle(deck)
+
 
 uporabnik1 = Uporabnik("Jaka")
-
 igralec1 = Igralec(uporabnik=uporabnik1, barva=3, lokacija=((0, 2), 7), karte_v_roki=[])
-
-
-
-for i, karta in enumerate(deck):
-    print(f"{i} --> {karta}")
+igra = Igra("prva igra", igralci=[igralec1])
