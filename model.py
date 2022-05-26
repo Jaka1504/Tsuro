e9444f20e875718fb3915dfd483fcfbe70742f05
Jaka Vrhovnik <jaka.vrhovnik2@gmail.com>
2022-05-26 23:04:46 +0200

    Razdelil atribut razreda `Igralec` `lokacija`

--- model.py
+++ model.py
@@ -43,7 +43,8 @@ class Karta:
 class Igralec:
     uporabnik: Uporabnik
     barva: int          # barve bi bile kar 0, 1, 2, ...
-    lokacija: tuple     # ((x, y), k) ... na ploščici s koordinatama x, y na položaju k
+    polje: tuple        # (x, y) ... na ploščici s koordinatama x, y
+    polozaj: int        # položaj na ploščici - int med 0 in 7
     karte_v_roki: List[Karta]
 
 
@@ -60,6 +61,39 @@ class Igra:
             self.tabela = {(x, y): None for x in range(VELIKOST_TABELE[0]) for y in range(VELIKOST_TABELE[1])}
         if self.kupcek is None:
             self.kupcek = []
+
+
+    def postavi_karto_na_tabelo(self, karta, polje):
+        self.tabela[polje] = karta
+    
+
+    def poisci_pot_od_tocke(self, polje, polozaj):
+        while not self.tabela[polje] is None:
+            novo = Igra.napreduj_po_tabeli(polje, polozaj, self.tabela[polje].povezave)
+            polje = novo["novo_polje"]
+            polozaj = novo["nov_polozaj"]
+        return (polje, polozaj)
+    
+
+    @staticmethod
+    def napreduj_po_tabeli(staro_polje, star_polozaj, povezave_karte):
+        x, y = staro_polje
+        nov_polozaj = povezave_karte[star_polozaj]
+        nov_polozaj = Igra.nasproten_polozaj(nov_polozaj)
+        if nov_polozaj in [0, 1]:
+            y += -1
+        elif nov_polozaj in [2, 3]:
+            x += -1
+        elif nov_polozaj in [4, 5]:
+            y += 1
+        else:
+            x += 1
+        return {"novo_polje": (x, y),
+                "nov_polozaj": nov_polozaj}
+    
+    @staticmethod
+    def nasproten_polozaj(polozaj):
+        return [5, 4, 7, 6, 1, 0, 3, 2][polozaj]
 
 
     @staticmethod
@@ -124,6 +158,14 @@ class Tsuro:
 
 
 
-uporabnik1 = Uporabnik("Jaka")
-igralec1 = Igralec(uporabnik=uporabnik1, barva=3, lokacija=((0, 2), 7), karte_v_roki=[])
-igra = Igra("prva igra", igralci=[igralec1])
+# uporabnik1 = Uporabnik("Jaka")
+# igralec1 = Igralec(uporabnik=uporabnik1, barva=3, polje=(0, 2), polozaj=7, karte_v_roki=[])
+# igra = Igra("prva igra", igralci=[igralec1])
+# igra.ustvari_nov_kupcek()
+# karta1 = igra.kupcek[0]
+# karta2 = igra.kupcek[1]
+# karta3 = igra.kupcek[2]
+for i, karta in enumerate(Igra.vse_karte()):
+    print(i, karta)
+
+karta = Karta([3, 4, 7, 0, 1, 6, 5, 2])
