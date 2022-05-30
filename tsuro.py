79388abe26b71e642dda71b851c71102539374dd
Jaka Vrhovnik <jaka.vrhovnik2@gmail.com>
2022-05-31 01:56:07 +0200

    Dodal statične datoteke

--- tsuro.py
+++ tsuro.py
@@ -5,6 +5,16 @@ import bottle
 tsuro = model.Tsuro()
 
 
+@bottle.get("/img/<file>")
+def staticne_slike(file):
+    return bottle.static_file(file, root="img")
+
+
+@bottle.get("/views/style.css")
+def css():
+    return bottle.static_file("style.css", root="views")
+
+
 @bottle.get("/")
 def osnovna_stran():
     return bottle.template("index")
